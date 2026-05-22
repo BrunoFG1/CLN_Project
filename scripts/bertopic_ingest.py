6bd0756deb594cbb3153f16ec78a9171aa2dddbb
Bruno Gonçalves <brunofarinhagoncalves@gmail.com>
2026-05-22 23:43:23 +0100

    Project finnished just have to do the interface

--- scripts/bertopic_ingest.py
+++ scripts/bertopic_ingest.py
@@ -3,25 +3,25 @@ from langchain_community.document_loaders import PyPDFLoader
 from bertopic import BERTopic
 import os
 from hdbscan import HDBSCAN
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.documents import Document
 
 path = "../data/"
 
 paragrafos = []
 
 def convert_text(doc):
-    lista = []
-    for i in doc:
-        lista.append(i.page_content)
-
-    text = "\n".join(lista)
-    paragrafos = [p.strip() for p in text.split("\n\n") if p.strip()]
-    return paragrafos
+    blocks = [page.page_content.strip() for page in doc if page.page_content.strip()]
+    return blocks
 
 for pdf in os.listdir(path):
-    doc = PyPDFLoader(path + pdf)
-    content = doc.lazy_load()
-    p = convert_text(content)
-    paragrafos.extend(p)
+    if pdf.endswith(".pdf"):
+        doc = PyPDFLoader(path + pdf)
+        content = doc.load()
+        p = convert_text(content)
+        paragrafos.extend(p)
+        print(f"li o pdf {pdf} e tem {len(content)}")
 
 model_sentence = SentenceTransformer("all-MiniLM-L6-v2", device="cuda")
 
@@ -35,8 +35,21 @@ model_topic = BERTopic(embedding_model=model_sentence, hdbscan_model=cluster_mod
 
 topics, probs = model_topic.fit_transform(paragrafos)
 
+updated_topics = model_topic.reduce_outliers(
+    paragrafos, 
+    topics, 
+    strategy="probabilities", 
+    probabilities=probs
+)
+model_topic.update_topics(paragrafos, topics=updated_topics)
+
+topics = updated_topics
+
 model_topic.save("../modelos/bertopic_model", serialization="safetensors", save_embedding_model=False)
 
+documents_chroma = [Document(page_content=texto) for texto in paragrafos]
 
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
+db = Chroma.from_documents(documents_chroma, embeddings, persist_directory="../db")
 
